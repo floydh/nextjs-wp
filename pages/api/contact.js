30cfc1b5a192ed2b12b2211cf03c6fb651e1876a
Floyd <floyd.hartford@gmail.com>
2020-11-21 17:59:26 -0500

    adding any untracked files

---
+++ pages/api/contact.js
@@ -0,0 +1,45 @@
+import nodemailer from "nodemailer"
+
+const emailPass = "cy~}b^iIu5Jb"
+// port 587?
+
+const transporter = nodemailer.createTransport({
+    host: "mail.floydhartford.com",
+    port: 25,
+    auth: {
+        user: "nextjsapp@floydhartford.com",
+        pass: emailPass
+    }
+})
+
+export default async (req, res) => {
+    const { senderMail, name, content, recipientMail } = req.body
+
+		console.warn(req.body);
+
+    // Check if fields are all filled
+    if (senderMail === "" || name === "" || content === "" || recipientMail === "") {
+        res.status(403).send("")
+        return
+    }
+
+    const mailerRes = await mailer({ senderMail, name, text: content, recipientMail })
+    res.send(mailerRes)
+}
+
+const mailer = ({ senderMail, name, text, recipientMail }) => {
+    const from = name && senderMail ? `${name} <${senderMail}>` : `${name || senderMail}`
+    const message = {
+        from,
+        to: `${recipientMail}`,
+        subject: `New message from ${from}`,
+        text,
+        replyTo: from
+    }
+
+    return new Promise((resolve, reject) => {
+        transporter.sendMail(message, (error, info) =>
+            error ? reject(error) : resolve(info)
+        )
+    })
+}
