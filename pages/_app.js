dde7229e8c6df1798809ca0eac3b8a945ffc9b78
Floyd <floyd.hartford@gmail.com>
2020-11-21 20:08:53 -0500

    removed statickit call

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,7 @@
-import { StaticKitProvider } from '@statickit/react';
 import '../styles/globals.css';
 
 export default function MyApp({ Component, pageProps }) {
   return (
-    <StaticKitProvider site="{your-site-id}">
       <Component {...pageProps} />
-    </StaticKitProvider>
   );
 }
